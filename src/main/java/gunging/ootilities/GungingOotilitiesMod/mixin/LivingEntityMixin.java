81af1b3138385baa9abf182ae32638f2b08bc80d
Gunging <gungingbross@gmail.com>
2025-06-09 19:47:08 -0400

    Minor fixes, increased version

--- src/main/java/gunging/ootilities/GungingOotilitiesMod/mixin/LivingEntityMixin.java
+++ src/main/java/gunging/ootilities/GungingOotilitiesMod/mixin/LivingEntityMixin.java
@@ -1,14 +1,31 @@
 package gunging.ootilities.GungingOotilitiesMod.mixin;
 
+import gunging.ootilities.GungingOotilitiesMod.events.ExtensionEventBroadcaster;
+import gunging.ootilities.GungingOotilitiesMod.events.extension.ItemFlowExtensionReason;
 import net.minecraft.world.entity.*;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity implements Attackable, net.minecraftforge.common.extensions.IForgeLivingEntity {
 
     public LivingEntityMixin(EntityType<?> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
+    }
+
+    @Inject(method = "handleEquipmentChanges", at = @At("RETURN"))
+    protected void onHandleEquipmentChangesReturn(Map<EquipmentSlot, ItemStack> pEquipments, CallbackInfo ci) {
+
+        // Send an update for every change here
+        for (Map.Entry<EquipmentSlot, ItemStack> slot : pEquipments.entrySet()) {
+            ExtensionEventBroadcaster.BroadcastEquipmentChangeEvent(ItemFlowExtensionReason.CLIENTBOUND_SET_EQUIPMENT_SEND_HANDLE_EQUIPMENT_CHANGES, false, slot.getKey(), (LivingEntity) (Object) this);
+        }
     }
 
     /*
